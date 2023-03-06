e75fdf3d79694d536c48074eb8adf7df356ab888
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-06 23:57:57 +0100

    return pods metadata

--- src/kuber.py
+++ src/kuber.py
@@ -6,12 +6,12 @@ def get_pods():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    ret = v1.list_namespaced_pod(namespace='app', watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-        print("Info: Pod name is -> " + i.metadata.name)
-        print("Info: Pod's logs:")
-        print(v1.read_namespaced_pod_log(i.metadata.name, 'app'))
+    ret = v1.list_namespaced_pod(namespace='kube-system', watch=False)
+    return (ret.metadata.name)
+    # for i in ret.items:
+    #     print("%s\t%s\t%s" %
+    #           (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    #     print("Info: Pod name is -> " + i.metadata.name)
+    #     print("Info: Pod's logs:")
+    #     print(v1.read_namespaced_pod_log(i.metadata.name, 'app'))
 
-        return (i.metadata.name)        
