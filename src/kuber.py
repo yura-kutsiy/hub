030bfb7ee9094544fa628dec588be1db2952c245
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-06 23:22:06 +0100

    get pod instead namespace

--- src/kuber.py
+++ src/kuber.py
@@ -2,11 +2,10 @@ from kubernetes import client, config
 
 config.load_incluster_config()
 
-def get_namespace():
+def get_pod():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
     ret = v1.list_namespaced_pod(namespace='app', watch=False)
     for i in ret.items:
         print("%s\t%s\t%s" %
@@ -14,7 +13,5 @@ def get_namespace():
         print("Info: Pod name is -> " + i.metadata.name)
         print("Info: Pod's logs:")
         print(v1.read_namespaced_pod_log(i.metadata.name, 'app'))
-        return ("Info: Pod name is -> " + i.metadata.name)        
 
-if __name__ == '__main__':
-    get_namespace()
+        return (i.metadata.name)        
