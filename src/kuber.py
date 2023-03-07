854e00ee74d3e04a27d1cc1faf201eee6391e887
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-07 23:11:28 +0100

    get pods improve

--- src/kuber.py
+++ src/kuber.py
@@ -6,12 +6,7 @@ def get_pods():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    ret = v1.list_namespaced_pod(namespace='kube-system', watch=False)
-    return (ret.metadata.name)
-    # for i in ret.items:
-    #     print("%s\t%s\t%s" %
-    #           (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-    #     print("Info: Pod name is -> " + i.metadata.name)
-    #     print("Info: Pod's logs:")
-    #     print(v1.read_namespaced_pod_log(i.metadata.name, 'app'))
+    pods = v1.list_namespaced_pod(namespace='app', watch=False)
+    return pods
 
+get_pods()
