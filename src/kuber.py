9b1a738c3fbd04c8f6e61cac5348c6ed1b8a0df5
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-08 11:52:01 +0100

    fix uncommited

--- src/kuber.py
+++ src/kuber.py
@@ -8,16 +8,12 @@ def get_pods():
     v1 = client.CoreV1Api()
     pods_list = v1.list_namespaced_pod(namespace='app', watch=False)
     pods = [item.metadata.name for item in pods_list.items]
-    print(pods)
-    # return pods
-get_pods()
+    return pods
+
 def get_namespaced_pods(namespace):
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
     pods_list = v1.list_namespaced_pod(f"namespace='{namespace}', watch=False")
     namespaced_pods = [item.metadata.name for item in pods_list.items]
-    print(namespaced_pods)
-    # return namespaced_pods
-
-get_namespaced_pods(namespace='kube-system')
+    return namespaced_pods
