5c98b51233715c81f9f8ff318206459268c50b46
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-08 09:02:09 +0100

    get_pods return list of pods name

--- src/kuber.py
+++ src/kuber.py
@@ -6,7 +6,6 @@ def get_pods():
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
-    pods = v1.list_namespaced_pod(namespace='app', watch=False)
+    pods_list = v1.list_namespaced_pod(namespace='app', watch=False)
+    pods = [item.metadata.name for item in pods_list.items]
     return pods
-
-get_pods()
