58b5a7584245bdc9af847a776805876da7521283
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-03-08 11:37:41 +0100

    swager parametr namespace

--- src/kuber.py
+++ src/kuber.py
@@ -8,12 +8,16 @@ def get_pods():
     v1 = client.CoreV1Api()
     pods_list = v1.list_namespaced_pod(namespace='app', watch=False)
     pods = [item.metadata.name for item in pods_list.items]
-    return pods
-
+    print(pods)
+    # return pods
+get_pods()
 def get_namespaced_pods(namespace):
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
     pods_list = v1.list_namespaced_pod(f"namespace='{namespace}', watch=False")
     namespaced_pods = [item.metadata.name for item in pods_list.items]
-    return namespaced_pods
+    print(namespaced_pods)
+    # return namespaced_pods
+
+get_namespaced_pods(namespace='kube-system')
