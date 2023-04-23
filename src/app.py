0805d1d2583ae1c0dedea2794e7651684d1e5808
yura-kutsiy <yuriy.kutsiy@eleks.com>
2023-04-23 22:36:18 +0200

    improve namespaced pods

--- src/app.py
+++ src/app.py
@@ -11,10 +11,9 @@ CORS(app)
 @app.route('/kuber/pods')
 def get_pods():
     config.load_incluster_config()
-
     v1 = client.CoreV1Api()
-    pods_list = v1.list_namespaced_pod(namespace='app', watch=False)
 
+    pods_list = v1.list_namespaced_pod(namespace='app', watch=False)
     pods = []
     for item in pods_list.items:
         pod = {
@@ -29,19 +28,19 @@ def get_pods():
 @app.route('/kuber/<namespace>/pods')
 def get_namespaced_pods(namespace):
     config.load_incluster_config()
-
     v1 = client.CoreV1Api()
+
     pods_list = v1.list_namespaced_pod(namespace=namespace, watch=False)
-    pods_data = []
-    for pod in pods_list.items:
-        pod_dict = pod.to_dict()
-        pod_data = {
-            "name": pod_dict['metadata']['name'],
-            "status": pod_dict['status']['phase'],
-            "age": pod_dict['metadata']['creation_timestamp']
+    pods = []
+    for item in pods_list.items:
+        pod = {
+            'name': item.metadata.name,
+            'status': item.status.phase,
+            'restarts': item.status.container_statuses[0].restart_count,
+            'age': (datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) - item.status.start_time).total_seconds()
         }
-        pods_data.append(pod_data)
-    return jsonify(pods_data)
+        pods.append(pod)
+    return jsonify(pods)
 
 @app.route('/kuber/<namespace>/<pod_name>/logs')
 def get_logs(pod_name, namespace):
