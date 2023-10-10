5bf12b13494fc40b7e388ccbdc07dc6993018364
Yura Kutsiy <yura.kutsiy@gmail.com>
2023-10-10 23:44:29 +0200

    rename class Services

--- src/Controllers/KubernetesController.cs
+++ src/Controllers/KubernetesController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using services;
+using Services;
 
 namespace kuberApi.Controllers
 {
--- src/KubernetesServices.cs
+++ src/KubernetesServices.cs
@@ -2,7 +2,7 @@ using k8s;
 using k8s.Models;
 using Config;
 
-namespace services
+namespace Services
 {
     public class KubernetesServiceDto
     {
