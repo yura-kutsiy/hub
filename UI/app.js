01f772774589343207d0e52b590f8c462ddcd689
Yura Kutsiy <yura.kutsiy@gmail.com>
2023-10-28 01:44:17 +0200

    update app.js

--- UI/app.js
+++ UI/app.js
@@ -2,52 +2,48 @@ const namespaceList = document.querySelector(".namespace-list");
 const divElement = document.getElementById("dashboard-name");
 
 const jsonData = {
-  "optional_name": "local-develop-cluster",
-  "namespaces": [
-    "app",
-    "argocd",
-    "tmp"
-  ]
+  optional_name: "local-develop-cluster",
+  namespaces: ["app", "argocd", "tmp"],
 };
 
 const namespaces = jsonData.namespaces;
 const optionalName = jsonData.optional_name;
 divElement.innerHTML = optionalName;
 
-namespaces.forEach((namespace) => {
-  const namespaceItem = document.createElement("div");
-  namespaceItem.classList.add("namespace-item");
-  namespaceItem.dataset.namespace = namespace;
-  namespaceItem.textContent = namespace;
-  namespaceList.appendChild(namespaceItem);
+if (namespaces?.length) {
+  namespaces.forEach((namespace) => {
+    const namespaceItem = document.createElement("div");
+    namespaceItem.classList.add("namespace-item");
+    namespaceItem.dataset.namespace = namespace;
+    namespaceItem.textContent = namespace;
+    namespaceList.appendChild(namespaceItem);
+  });
+}
 
-  // Get the namespace-title element
-  const namespaceTitle = document.getElementById("namespace-title");
-
-  // Add an event listener to each namespace-item element
-  const namespaceItems = document.querySelectorAll(".namespace-item");
-  console.log(namespaceItems);
-  namespaceItems
-    .forEach((item) => {
-      item.addEventListener("click", () => {
-        console.log(item);
-        // Get the namespace from the data-attribute
-        const namespace = item.dataset.namespace;
-        // Update the namespace-title element with the clicked namespace
-        namespaceTitle.textContent = namespace + " ns";
-        // Fetch the pods for the clicked namespace
-        const apiUrl = `/kuber/${namespace}/pods`;
-        fetch(apiUrl)
-          .then((response) => response.json())
-          .then((data) => {
-            // Render the pod list
-            const podList = document.querySelector(".pod-list");
-            podList.innerHTML = "";
-            const podListHeader = document.createElement("h2");
-            podListHeader.textContent = "Pods";
-            podList.appendChild(podListHeader);
-            const table = document.createElement("table");
-            const tableHeader = `
+// Get the namespace-title element
+const namespaceTitle = document.getElementById("namespace-title");
+// Add an event listener to each namespace-item element
+const namespaceItems = document.querySelectorAll(".namespace-item");
+if (namespaceItems?.length) {
+  namespaceItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      // Get the namespace from the data-attribute
+      const namespace = item.dataset.namespace;
+      // Update the namespace-title element with the clicked namespace
+      namespaceTitle.textContent = namespace + " ns";
+      // Fetch the pods for the clicked namespace
+      const apiUrl = `/kuber/${namespace}/pods`;
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          // Render the pod list
+          const podList = document.querySelector(".pod-list");
+          podList.innerHTML = "";
+          const podListHeader = document.createElement("h2");
+          podListHeader.textContent = "Pods";
+          podList.appendChild(podListHeader);
+          const table = document.createElement("table");
+          const tableHeader = `
               <tr>
                 <th>Name</th>
                 <th>Status</th>
@@ -55,7 +51,8 @@ namespaces.forEach((namespace) => {
                 <th>Age</th>
               </tr>
             `;
-            table.innerHTML = tableHeader;
+          table.innerHTML = tableHeader;
+          if (data?.length) {
             data.forEach((pod) => {
               const ageInSeconds = pod.age;
               let ageString = "";
@@ -87,17 +84,16 @@ namespaces.forEach((namespace) => {
               `;
               table.innerHTML += tableRow;
             });
-            podList.appendChild(table);
-          })
-          .catch((error) => {
-            console.error(
-              `Error fetching data for namespace ${namespace}:`,
-              error
-            );
-          });
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching namespaces:", error);
+          }
+
+          podList.appendChild(table);
+        })
+        .catch(() => {
+          console.error(
+            `Error fetching data for namespace ${namespace}:,
+              error`
+          );
+        });
     });
-});
+  });
+}
