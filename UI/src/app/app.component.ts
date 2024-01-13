6e7b640f7d6594e94f31c19ab80f310e6bac31de
Yura Kutsiy <yura.kutsiy@gmail.com>
2024-01-13 22:26:11 +0100

    first step

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import * as namespaces from '../assets/namespaces.json';
-
+import { HttpClient } from '@angular/common/http';
+import * as sharedConfig from '../assets/namespaces.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'UI';
-  ngOnInit(): void {
-    console.log(namespaces);
 
+export class AppComponent implements OnInit {
+  sharedConfig = sharedConfig;
+  podsInfo: {
+    ageInSeconds: number;
+    name: string;
+    restarts: number;
+    status: string;
+  }[] = [];
+
+  constructor(public httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    console.log(sharedConfig);
   }
+
+  getPodsInfo(namespace: string) {
+    console.log(namespace);
+    this.httpClient.get('http://192.168.0.28:31135/kuber/' + namespace + '/pods').subscribe((podsInfo: any) => {
+      console.log(podsInfo);
+      this.podsInfo = podsInfo;
+    });
+  }
+
 }
